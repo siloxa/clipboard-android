cf3d28dc9f22361b6cd191e416328b5b03d82d1f
Beny <behnammnasehi@gmail.com>
2024-03-11 09:54:22 +0330

    update dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,6 +16,16 @@ android {
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
+    buildFeatures {
+        dataBinding = true
+        viewBinding = true
+    }
+
+    packaging {
+        resources {
+            excludes +="META-INF/rxjava.properties"
+        }
+    }
 
     buildTypes {
         release {
@@ -44,4 +54,24 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    implementation("com.jakewharton.retrofit:retrofit2-rxjava2-adapter:1.0.0")
+    implementation("com.squareup.retrofit2:adapter-rxjava:2.9.0")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-scalars:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.okhttp3:okhttp:5.0.0-alpha.12")
+    implementation("com.squareup.okhttp3:logging-interceptor:5.0.0-alpha.12")
+
+    implementation("androidx.datastore:datastore-preferences:1.0.0")
+
+    implementation("com.squareup.picasso:picasso:2.71828")
+
+    implementation("com.github.razir.progressbutton:progressbutton:2.1.0")
+
+    implementation("androidx.annotation:annotation-jvm:1.7.1")
+
+    implementation("io.socket:socket.io-client:2.0.0") {
+        exclude(group = "org.json", module = "json")
+    }
 }
